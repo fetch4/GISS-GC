91b10ada9f1232dbde69eccc231313ca74f4fba5
jwallwork23 <jw2423@cam.ac.uk>
2024-09-12 14:08:13 +0100

    ifdef for CNV_FRC

--- model/CHEM_DRV.F90
+++ model/CHEM_DRV.F90
@@ -392,8 +392,10 @@ CONTAINS
           ! Surface roughness height [m]
           State_Met%Z0          (II,JJ) = z0m_save(i,j)                                             
 
+#ifdef MODEL_GEOS
           ! Convective fraction [1] (only used by GEOS)
-          State_Met%CNV_FRC     (II,JJ) = 0.0                                                       
+          State_Met%CNV_FRC     (II,JJ) = 0.0
+#endif
 
        ENDDO
     ENDDO
