057f75cb98c8992dd4e7fb3bb4ea2ffcb3950d10
Joe Wallwork <jw2423@cam.ac.uk>
2024-11-27 11:29:18 +0000

    Reorder build to set up configuration files afterwards

--- .dev/utils/build.sh
+++ .dev/utils/build.sh
@@ -63,20 +63,6 @@ if [ "${GISS_ONLY}" = true ]; then
 else
   GC=YES
   RUNID=GISS_GC_14
-  # Copy over configuration files
-  HUGE_SPACE=${ModelE_Support}/huge_space/${RUNID}
-  PROD_RUNS=${ModelE_Support}/prod_runs/${RUNID}
-  CONFIG=${GISS_HOME}/.dev/config
-  for RUNDIR in ${HUGE_SPACE} ${PROD_RUNS}; do
-    ln -s -f ${CONFIG}/geoschem_config.yml ${RUNDIR}/geoschem_config.yml
-    ln -s -f ${CONFIG}/HEMCO_Config.rc ${RUNDIR}/HEMCO_Config.rc
-    ln -s -f ${CONFIG}/HEMCO_Diagn.rc ${RUNDIR}/HEMCO_Diagn.rc
-    ln -s -f ${CONFIG}/HISTORY.rc ${RUNDIR}/HISTORY.rc
-    ln -s -f ${CONFIG}/species_database.yml ${RUNDIR}/species_database.yml
-  done
-  # Create output directories
-  mkdir -p ${HUGE_SPACE}/OutputDir
-  mkdir -p ${PROD_RUNS}/OutputDir
 fi
 
 # Print the values for verification
@@ -103,4 +89,21 @@ if [ "${DEBUG}" = true ]; then
 else
   make -j setup RUN=${RUNID} F90=mpif90 GC=${GC} MP=${OPENMP} MPI=YES MECH=carbon \
     TYPE=Release
+fi
+
+if [ "${GISS_ONLY}" = false ]; then
+  # Copy over configuration files
+  HUGE_SPACE=${ModelE_Support}/huge_space/${RUNID}
+  PROD_RUNS=${ModelE_Support}/prod_runs/${RUNID}
+  CONFIG=${GISS_HOME}/.dev/config
+  for RUNDIR in ${HUGE_SPACE} ${PROD_RUNS}; do
+    ln -s -f ${CONFIG}/geoschem_config.yml ${RUNDIR}/geoschem_config.yml
+    ln -s -f ${CONFIG}/HEMCO_Config.rc ${RUNDIR}/HEMCO_Config.rc
+    ln -s -f ${CONFIG}/HEMCO_Diagn.rc ${RUNDIR}/HEMCO_Diagn.rc
+    ln -s -f ${CONFIG}/HISTORY.rc ${RUNDIR}/HISTORY.rc
+    ln -s -f ${CONFIG}/species_database.yml ${RUNDIR}/species_database.yml
+  done
+  # Create output directories
+  mkdir -p ${HUGE_SPACE}/OutputDir
+  mkdir -p ${PROD_RUNS}/OutputDir
 fi
