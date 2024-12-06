37ea76d20da8fdee74d77fcf3e851c811e1ccad8
Joe Wallwork <jw2423@cam.ac.uk>
2024-12-06 15:17:49 +0000

    More concise ifdefs

--- model/SUBDD.f
+++ model/SUBDD.f
@@ -1824,17 +1824,12 @@ c add (calls to) the analogs of ijh_defs et al.
       catshapes(k) = 'aijlh'; categories(k) = 'taijlh'
       input_sizes3(k) = lm
       call tijlh_defs(diaglists(1,k),nmax_possible,diaglens(k))
-#endif
 
-#ifdef TRACERS_GC
       k = k + 1
       catshapes(k) = 'aijlh'; categories(k) = 'taijlh'
       input_sizes3(k) = lm
       call tijlh_defs(diaglists(1,k),nmax_possible,diaglens(k))
-#endif
 
-#ifdef TRACERS_GC
-      
       k = k + 1
       catshapes(k) = 'aijlh'; categories(k) = 'taijlh'
       input_sizes3(k) = lm
@@ -1844,10 +1839,9 @@ c add (calls to) the analogs of ijh_defs et al.
       catshapes(k) = 'aijh'; categories(k) = 'taijh'
       input_sizes3(k) = 0
       call tijh_defs(diaglists(1,k),nmax_possible,diaglens(k))
-      
 #endif
 
-      
+
 c
 c check whether each requested diagnostic is in the list
 c of declared possible outputs
