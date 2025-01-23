4653b0666dfc7a1d864a8158ae715906f7cb0a8c
Joe Wallwork <jw2423@cam.ac.uk>
2025-01-23 14:50:10 +0000

    Fix GEOS-Chem rundir

--- .dev/utils/setup.sh
+++ .dev/utils/setup.sh
@@ -8,9 +8,10 @@
 export GISS_HOME=${HOME}/software/GISS-GC
 # NOTE: Path may need to be edited for your system
 export ModelE_Support=${HOME}/run/GISS-GC
-# NOTE: Path may need to be edited for your system
-export GCCLASSIC_RUNDIR=${HOME}/run/gcclassic/gc_4x5_merra2_fullchem
 mkdir -p ${ModelE_Support}
+# NOTE: Path may need to be edited for your system
+export GCCLASSIC_RUNDIR=${HOME}/run/gcclassic
+mkdir -p ${GCCLASSIC_RUNDIR}
 # Environment variables for compiler
 export CC=gcc      # NOTE: C compiler may need to be modified for your system
 export CXX=g++     # NOTE: C++ compiler may need to be modified for your system
