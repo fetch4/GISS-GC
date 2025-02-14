904343ac23f49eadbfd220aaba3638fb430a0466
Joe Wallwork <jw2423@cam.ac.uk>
2025-02-14 10:56:17 +0000

    Turn off categories beginning with t under TRACERS_GC in SUBDD.f

--- model/SUBDD.f
+++ model/SUBDD.f
@@ -1819,18 +1819,6 @@ c add (calls to) the analogs of ijh_defs et al.
       input_sizes3(k) = lm
       call tijph_defs(diaglists(1,k),nmax_possible,diaglens(k))
 #endif
-#ifdef TRACERS_GC
-      k = k + 1
-      catshapes(k) = 'aijlh'; categories(k) = 'taijlh'
-      input_sizes3(k) = lm
-      call tijlh_defs(diaglists(1,k),nmax_possible,diaglens(k))
-
-      k = k + 1
-      catshapes(k) = 'aijh'; categories(k) = 'taijh'
-      input_sizes3(k) = 0
-      call tijh_defs(diaglists(1,k),nmax_possible,diaglens(k))
-#endif
-
 
 c
 c check whether each requested diagnostic is in the list
