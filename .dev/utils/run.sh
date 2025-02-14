3ba6c0611b82656ca1c09024a3c1344e7d732eee
Joe Wallwork <jw2423@cam.ac.uk>
2025-02-14 14:28:04 +0000

    Log stderr as well as stdout

--- .dev/utils/run.sh
+++ .dev/utils/run.sh
@@ -115,9 +115,9 @@ else
   ./${RUNID}ln
   if [ "${COLD_RESTART}" = true ]; then
     # Run the model for one hour
-    mpiexec -np "${NP}" ./${RUNID}.exe -i I -cold-restart | tee cold-restart.log
+    mpiexec -np "${NP}" ./${RUNID}.exe -i I -cold-restart 2>&1 | tee cold-restart.log
   else
     # Pick up from a checkpoint and run the model for the full duration
-    mpiexec -np "${NP}" ./${RUNID}.exe -i I | tee "${RUNID}.PRT"
+    mpiexec -np "${NP}" ./${RUNID}.exe -i I 2>&1 | tee "${RUNID}.PRT"
   fi
 fi
