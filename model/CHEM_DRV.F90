40a46bd14872390da0b7699fd4213455ea877143
Lee T. Murray <lee.murray@rochester.edu>
2025-03-07 13:55:29 -0500

    Enable SUBDD to archive any species, not just advected ones.

--- model/CHEM_DRV.F90
+++ model/CHEM_DRV.F90
@@ -1408,7 +1408,7 @@ CONTAINS
     USE CONSTANT,                ONLY : Pi
     USE MODEL_COM,               ONLY : DTsrc
     USE Dictionary_mod,          ONLY : sync_param
-    USE CHEM_COM,                ONLY : SpcChmID_to_TrID, t_qlimit, TrFullName, TrID_to_SpcChmID
+    USE CHEM_COM,                ONLY : SpcChmID_to_TrID, t_qlimit, TrFullName, TrID_to_SpcChmID, NSP, SpName
     USE ERROR_MOD,               ONLY : Debug_Msg, Error_Stop, Init_Error
 
     USE GC_Environment_Mod,      ONLY : GC_Allocate_All
@@ -1447,7 +1447,6 @@ CONTAINS
 
     INTEGER   :: I, J, L, N, NN, II, JJ, I_0H, I_1H
     INTEGER   :: NYMDb, NHMSb, NHMSe
-    INTEGER   :: NSP
     INTEGER   :: id_H2O, id_CH4, id_CLOCK
 
     INTEGER   :: TAU, TAUb
@@ -2042,13 +2041,14 @@ CONTAINS
     !-----------------------------------------------------------------------------
     
     NSP = State_Chm%nSpecies
-    NTM = State_Chm%nAdvect + 1
-
+    NTM = State_Chm%nAdvect
+    
     ALLOCATE( TrID_to_SpcChmID(NTM) )
     ALLOCATE( SpcChmID_to_TrID(NSP) )
     TrID_to_SpcChmID = 0
     SpcChmID_to_TrID = 0
 
+    ALLOCATE( SpName(NSP) )
     ALLOCATE( TrName(NTM) )
     ALLOCATE( TrFullName(NTM) )
     ALLOCATE( IsAdvected(NTM) )
@@ -2058,8 +2058,8 @@ CONTAINS
 
     NN=1
     DO N = 1, NSP
-       IF ( State_Chm%SpcData(N)%Info%Is_Advected .or. &
-            TRIM( State_Chm%SpcData(N)%Info%Name ) .eq. "OH" ) THEN
+       SpName(N) = TRIM( State_Chm%SpcData(N)%Info%Name )
+       IF ( State_Chm%SpcData(N)%Info%Is_Advected ) THEN
  
           TrName(NN) =     TRIM( State_Chm%SpcData(N)%Info%Name )
           TrFullName(NN) = TRIM( State_Chm%SpcData(N)%Info%FullName ) // " (" // &
@@ -2090,10 +2090,10 @@ CONTAINS
           ENDDO
        ENDDO
     ENDDO
-
+    
     ! Return success
     RC = GC_SUCCESS
-
+    
     RETURN
   END SUBROUTINE INIT_CHEM
 
@@ -2999,35 +2999,23 @@ SUBROUTINE tijlh_defs(arr,nmax,decl_count)
 use subdd_mod, only : info_type
 ! info_type_ is a homemade structure constructor for older compilers
 use subdd_mod, only : info_type_
-use chem_com, only : ntm, trname
+use chem_com, only : ntm, trname, nsp, spname
 implicit none
 integer :: nmax,decl_count
 integer :: n
-character*80 :: unitString
 type(info_type) :: arr(nmax)
 
 decl_count = 0
 
 ! First, diagnostics available for all tracers:
-do n=1,ntm
+do n=1,nsp
    ! 3D mixing ratios (SUBDD string is just tracer name):
-  unitString='mol mol-1'
-  arr(next()) = info_type_(                      &
-       sname = trim(trname(n)),                  &
-       lname = trim(trname(n))//' mixing ratio', &
-       units = trim(unitString)                  &
+   arr(next()) = info_type_(                      &
+       sname = trim(spname(n)),                  &
+       lname = trim(spname(n))//' mixing ratio', &
+       units = 'mol mol-1'                       &
        )
 end do ! tracers loop
-
-!do n=1,State_Chm%nSpecies
-!   ! 3D mixing ratios (SUBDD string is just tracer name):
-!   unitString='mol mol-1'
-!   arr(next()) = info_type_(                                           &
-!        sname = trim(State_Chm%SpcData(N)%Info%Name),                  &
-!        lname = trim(State_Chm%SpcData(N)%Info%Name)//' mixing ratio', &
-!        units = trim(unitString)                  &
-!        )
-!end do ! tracers loop
 
 return
 contains
