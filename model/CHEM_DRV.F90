8b8fa206cb98427fb88dd9ac01ff75154441ab5f
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-14 11:06:38 +0000

    PHIS

--- model/CHEM_DRV.F90
+++ model/CHEM_DRV.F90
@@ -2501,6 +2501,16 @@ CONTAINS
              ENDDO
           endif
 
+          if ( trim(subdd%name(k)) == "StateMet_PHIS" ) then
+             DO J=J_0,J_1
+             DO I=I_0,I_1
+                II = I - I_0 + 1
+                JJ = J - J_0 + 1
+                sddarr2d(I,J) = State_Met%PHIS(II,JJ)
+             ENDDO
+             ENDDO
+          endif
+
           if ( trim(subdd%name(k)) == "StateMet_SUNCOSmid" ) then
              DO J=J_0,J_1
              DO I=I_0,I_1
@@ -3468,6 +3478,12 @@ decl_count = 0
   arr(next()) = info_type_(                      &
        sname = 'StateMet_PBLH',                  &
        lname = 'StateMet_PBLH',                  &
+       units = 'm'                               &
+       )
+
+  arr(next()) = info_type_(                      &
+       sname = 'StateMet_PHIS',                  &
+       lname = 'StateMet_PHIS',                  &
        units = 'm'                               &
        )
 
