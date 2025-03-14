4e1f18311570872bec85686a21d6fd942f733963
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-14 10:45:14 +0000

    FRSEAICE

--- model/CHEM_DRV.F90
+++ model/CHEM_DRV.F90
@@ -2409,6 +2409,16 @@ CONTAINS
              ENDDO
           endif
 
+          if ( trim(subdd%name(k)) == "StateMet_FRSEAICE" ) then
+             DO J=J_0,J_1
+             DO I=I_0,I_1
+                II = I - I_0 + 1
+                JJ = J - J_0 + 1
+                sddarr2d(I,J) = State_Met%FRSEAICE(II,JJ)
+             ENDDO
+             ENDDO
+          endif
+
           if ( trim(subdd%name(k)) == "StateMet_SUNCOSmid" ) then
              DO J=J_0,J_1
              DO I=I_0,I_1
@@ -3340,6 +3350,12 @@ decl_count = 0
   arr(next()) = info_type_(                      &
        sname = 'StateMet_FROCEAN',               &
        lname = 'StateMet_FROCEAN',               &
+       units = '1',                              &
+       )
+
+  arr(next()) = info_type_(                      &
+       sname = 'StateMet_FRSEAICE',              &
+       lname = 'StateMet_FRSEAICE',              &
        units = '1',                              &
        )
 
