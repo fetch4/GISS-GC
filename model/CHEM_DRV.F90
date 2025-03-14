0491f1e997b2f6df2aaa074e974b66fd7f3cfb04
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-14 09:45:02 +0000

    Hook up ChemGridLev and CLDFRC

--- model/CHEM_DRV.F90
+++ model/CHEM_DRV.F90
@@ -227,10 +227,10 @@ CONTAINS
           State_Met%AREA_M2     (II,JJ) = axyp(i,j)
 
           ! Chemistry grid level [1]
-          State_Met%ChemGridLev (II,JJ) = LM             
+          State_Met%ChemGridLev (II,JJ) = LM
 
           ! Column cloud fraction [1]
-          State_Met%CLDFRC      (II,JJ) = cfrac(i,j)     
+          State_Met%CLDFRC      (II,JJ) = cfrac(i,j)
 
 #ifdef CALC_MERRA2_LIKE_DIAGS
           ! Max cloud top height [levels]
@@ -2296,6 +2296,26 @@ CONTAINS
              ENDDO
           endif
 
+          if ( trim(subdd%name(k)) == "StateMet_ChemGridLev" ) then
+             DO J=J_0,J_1
+             DO I=I_0,I_1
+                II = I - I_0 + 1
+                JJ = J - J_0 + 1
+                sddarr2d(I,J) = State_Met%ChemGridLev(II,JJ)
+             ENDDO
+             ENDDO
+          endif
+
+          if ( trim(subdd%name(k)) == "StateMet_CLDFRC" ) then
+             DO J=J_0,J_1
+             DO I=I_0,I_1
+                II = I - I_0 + 1
+                JJ = J - J_0 + 1
+                sddarr2d(I,J) = State_Met%CLDFRC(II,JJ)
+             ENDDO
+             ENDDO
+          endif
+
           if ( trim(subdd%name(k)) == "StateMet_SUNCOSmid" ) then
              DO J=J_0,J_1
              DO I=I_0,I_1
@@ -3143,6 +3163,18 @@ type(info_type) :: arr(nmax)
 decl_count = 0
 
 !do n=1,1
+
+  arr(next()) = info_type_(                      &
+       sname = 'StateMet_ChemGridLev',           &
+       lname = 'StateMet_ChemGridLev',           &
+       units = '1'                               &
+       )
+
+  arr(next()) = info_type_(                      &
+       sname = 'StateMet_CLDFRC',                &
+       lname = 'StateMet_CLDFRC',                &
+       units = '1'                               &
+       )
 
   arr(next()) = info_type_(                      &
        sname = 'StateMet_SUNCOSmid',             &
