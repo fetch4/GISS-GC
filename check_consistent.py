1349788e97c39d830ad911b1c66ecbda907cde46
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-18 16:57:17 +0000

    Check COLLECTIONS are consistent

--- check_consistent.py
+++ check_consistent.py
@@ -7,13 +7,51 @@
 #   ./check_consistent.py
 # """
 
+# Define the two models 'gc' and 'giss' and their corresponding configuration files
 filenames = {"gc": "HISTORY.rc", "giss": "HISTORY_ModelE.rc"}
-lines = {}
+models = tuple(filenames.keys())
 
+
+def other_model(model):
+    """
+    Given model 'gc' or 'giss', return the other one.
+
+    :arg model: model name
+    :type model: str
+    """
+    return "gc" if model == "giss" else "giss"
+
+
+# Read the two configuration files and report the number of lines in each
+lines = {}
 for model, filename in filenames.items():
     with open(filename, "r") as f:
         lines[model] = f.readlines()
-
     print(f"Number of lines in {filename:20s}: {len(lines[model])}")
 
-# TODO: Actually check the files are consistent
+# Check that the COLLECTIONS used in each file are consistent
+collections = {}
+for model in models:
+    found = False
+    collections[model] = set()
+    for line in lines[model]:
+        if line.startswith("COLLECTIONS:"):
+            found = True
+            line = line.replace("COLLECTIONS:", "")
+        if not found:
+            continue
+        line = line.replace(" ", "").replace("\n", "").replace("'", "").replace(",", "")
+        if line.startswith("::"):
+            break
+        if line.startswith("#"):
+            continue
+        collections[model].add(line)
+for model in models:
+    other = other_model(model)
+    if not collections[model].issubset(collections[other]):
+        raise ValueError(
+            f"{filenames[model]} contains collections not in {filenames[other]}:"
+            f" {collections[model].difference(collections[other])}"
+        )
+
+# TODO: Further checks
