da77488a3f16dbd916f80e95b2caec7bd26412e8
Joe Wallwork <jw2423@cam.ac.uk>
2025-03-31 10:50:49 +0100

    Convert Model E -> GEOS-Chem transfers to TODOs

--- model/CHEM_DRV.F90
+++ model/CHEM_DRV.F90
@@ -624,56 +624,56 @@ CONTAINS
              if(hasnorthpole(grid) .and. JJJ .eq. J_1 ) I = 1
 
              ! TODO: Prod_?PRD? - NOTE: Loop case
-             
+
              ! Production of hydrophilic black carbon from hydrophobic black carbon [kg]
-             ProdLoss%ProdBCPIfromBCPO             (II,JJ,K) = prodbcpifrombcpo(i,j,k)
-        
+             ! TODO: ProdLoss%ProdBCPIfromBCPO             (II,JJ,K) = ???
+
              ! Production of hydrophilic organic carbon from hydrophobic organic carbon [kg]
-             ProdLoss%ProdOCPIfromOCPO             (II,JJ,K) = prodocpifromocpo(i,j,k)
+             ! TODO: ProdLoss%ProdOCPIfromOCPO             (II,JJ,K) = ???
 
              ! Production of HMS from aqueous reaction of SO2 and HCHO in clouds [kg S s-1]
-             ProdLoss%ProdHMSfromSO2andHCHOinCloud             (II,JJ,K) = prodhmsfromso2andhchoincloud(i,j,k)
+             ! TODO: ProdLoss%ProdHMSfromSO2andHCHOinCloud             (II,JJ,K) = ???
 
              ! Production of SO2 and HCHO from aqueous reaction of HS and OH- in clouds [kg S s-1]
-             ProdLoss%ProdSO2andHCHOfromHMSinCloud             (II,JJ,K) = prodso2andhchofromhmsincloud(i,j,k)
+             ! TODO: ProdLoss%ProdSO2andHCHOfromHMSinCloud             (II,JJ,K) = ???
 
              ! Production of SO4 from aqueous oxidation of O3 in clouds [kg S  s-1]
-             ProdLoss%ProdSO4fromHMSinCloud             (II,JJ,K) = prodso4fromhmsincloud(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromHMSinCloud             (II,JJ,K) = ???
 
              ! Production of SO4 from aqueous oxidation of H2O2 in clouds [kg S s-1]
-             ProdLoss%ProdSO4fromH2O2inCloud             (II,JJ,K) = prodso4fromh2o2incloud(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromH2O2inCloud             (II,JJ,K) = ???
 
              ! Production of SO4 from aqueous oxidation of O2 from metals in cloud [kg S]
-             ProdLoss%ProdSO4fromO2inCloudMetal             (II,JJ,K) = prodso4fromo2incloudmetal(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromO2inCloudMetal             (II,JJ,K) = ???
 
              ! Production of SO4 from aqueus oxidation of O3 in cloud [kg S s-1]
-             ProdLoss%ProdSO4fromO3inCloud             (II,JJ,K) = prodso4fromo3incloud(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromO3inCloud             (II,JJ,K) = ???
 
              ! Production of SO4 from O3 in sea salt [kg S s-1]
-             ProdLoss%ProdSO4fromO3inSeaSalt             (II,JJ,K) = prodso4fromo3inseasalt(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromO3inSeaSalt             (II,JJ,K) = ???
 
              ! Production of SO4 from aqueus oxidation of HOBr in clouds [kg S s-1]
-             ProdLoss%ProdSO4fromHOBrInCloud             (II,JJ,K) = prodso4fromhobrincloud(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromHOBrInCloud             (II,JJ,K) = ???
 
              ! Production of SO4 from sulphur production rate of O3 [kg S s-1]
-             ProdLoss%ProdSO4fromSRO3             (II,JJ,K) = prodso4fromsro3(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromSRO3             (II,JJ,K) = ???
 
              ! Production of SO4 from sulphur production rate of HOBr + O3 [kg S s-1]
-             ProdLoss%ProdSO4fromSRHObr             (II,JJ,K) = prodso4fromsrhobr(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromSRHObr             (II,JJ,K) = ???
 
              ! Production of SO4 from aqueous phase SO3 loss by OH [kg S s-1]
-             ProdLoss%ProdSO4fromO3s             (II,JJ,K) = prodso4fromo3s(i,j,k)
+             ! TODO: ProdLoss%ProdSO4fromO3s             (II,JJ,K) = ???
 
              ! TODO: Loss_?LOS? - NOTE: Loop case
 
              ! Loss of HNO3 on sea salt aerosols [kg s-1]
-             ProdLoss%LossHNO3onSeaSalt             (II,JJ,K) = losshno3onseasalt(i,j,k)
+             ! TODO: ProdLoss%LossHNO3onSeaSalt             (II,JJ,K) = ???
 
              ! Production of CO from CH4 [kg s-1]
-             ProdLoss%ProdCOfromCH4             (II,JJ,K) = prodcofromch4(i,j,k)
+             ! TODO: ProdLoss%ProdCOfromCH4             (II,JJ,K) = ???
 
              ! Production of CO from NMVOCs SO3 - loss by OH [kg s-1]
-             ProdLoss%ProdCOfromNMVOC             (II,JJ,K) = prodcofromnmvoc(i,j,k)
+             ! TODO: ProdLoss%ProdCOfromNMVOC             (II,JJ,K) = ???
           ENDDO
        ENDDO
     ENDDO
